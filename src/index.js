e270cf15594b4bcacf4b44fb35e56c20545698f1
Yehong Piao <piaoyehong1107@gmail.com>
2020-10-07 04:35:01 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // ./src/index.js
+// eslint-disable-next-line
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,5 +7,7 @@ import { createStore } from 'redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
+
+const store = createStore(shoppingListItemReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
